572b675e3c65379f68d3454a729762d62f1eae60
quyp <1095698022@qq.com>
2022-02-15 16:18:40 +0800

    feat:解析插值

--- src/compiler-core/src/parse.ts
+++ src/compiler-core/src/parse.ts
@@ -1,16 +1,71 @@
+import { NodeTypes } from "./ast";
+const openDelimiter = "{{";
+const closeDelimiter = "}}";
+function createParserContext(content: string) {
+  return {
+    source: content,
+  };
+  //
+}
+
 export function baseParse(content: string) {
   //
-  console.log(content);
+  // 创建一个全局的上下文对象
+  const context = createParserContext(content);
+
+  return createRoot(parseChildren(context));
+}
+function parseChildren(context) {
+  const nodes: any = [];
+  let node: any;
+  if (context.source.startsWith(openDelimiter)) {
+    node = parseInterpolation(context);
+  }
+  nodes.push(node);
+  return nodes;
+}
+
+function parseInterpolation(context) {
+  // {{message}} 获取到message内容
+  // 从index为2开始查找，前面0，1是固定的 {{ ,减少没必要的查询,所以indexof 第二个参数为open的长度
+  // 去除前面固定 {{ 的叫法为 推进
+  // 最后一个index
+  const closeIndex = context?.source.indexOf(
+    closeDelimiter,
+    openDelimiter.length
+  );
+  //
+  //   context.source = context?.source.substr(0, closeIndex);
+  // 先删除掉前面的
+  //   context.source = context?.source.slice(openDelimiter.length);
+  advanceBy(context, openDelimiter.length);
+  // 最终解析出来内容的长度
+  const rawContentLength = closeIndex - openDelimiter.length;
+
+  const rawContent = context.source.slice(0, rawContentLength);
+  const content = rawContent.trim();
+
+  advanceBy(context, rawContentLength + closeDelimiter.length);
+  //   context.source = context.source.slice(
+  //     rawContentLength + closeDelimiter.length
+  //   );
+  console.log("context", context.source);
 
   return {
-    children: [
-      {
-        type: "interpolation",
-        content: {
-          type: "simple_expression",
-          content: "message",
-        },
-      },
-    ],
+    type: NodeTypes.INTERPOLATION,
+    content: {
+      type: NodeTypes.SIMPLE_EXPRESSION,
+      content,
+    },
+  };
+}
+
+function advanceBy(context: any, length: number) {
+  context.source = context?.source.slice(length);
+}
+// 创建根结点
+function createRoot(children) {
+  return {
+    children,
   };
 }
--- src/compiler-core/tests/parse.spec.ts
+++ src/compiler-core/tests/parse.spec.ts
@@ -1,5 +1,5 @@
 import { baseParse } from "../src/parse";
-
+import { NodeTypes } from "../src/ast";
 describe("Parse", () => {
   describe("interpolation", () => {
     // 插值
@@ -11,9 +11,9 @@ describe("Parse", () => {
       // 验证当前ast是根结点,有children，插值节点位置在children[0]？？
       // toStrictEqual测试对象是否具有相同的类型和结构
       expect(ast.children[0]).toStrictEqual({
-        type: "interpolation",
+        type: NodeTypes.INTERPOLATION,
         content: {
-          type: "simple_expression",
+          type: NodeTypes.SIMPLE_EXPRESSION,
           content: "message",
         },
       });
